dc1320441078b1be89534b6fb883c0556f7334c2
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-11-06 14:21:06 -0800

    Add comments to functions

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Functionality for button 1
+// If txt2 or txt3 are visible, fade them out, then fade in txt1
 $('.btn1').click(function(){
   if ($('.txt2').is(":visible")) {
     $('.txt2').fadeOut(1000, function(){$('.txt1').fadeIn(1000)});
@@ -9,6 +11,8 @@ $('.btn1').click(function(){
   }
 });
 
+// Functionality for button 2
+// If txt1 or txt3 are visible, fade them out, then fade in txt2
 $('.btn2').click(function(){
   if ($('.txt1').is(":visible")) {
     $('.txt1').fadeOut(1000, function(){$('.txt2').fadeIn(1000)});
@@ -20,6 +24,8 @@ $('.btn2').click(function(){
   }
 });
 
+// Functionality for button 3
+// If txt1 or txt2 are visible, fade them out, then fade in txt3
 $('.btn3').click(function(){
   if ($('.txt1').is(":visible")) {
     $('.txt1').fadeOut(1000, function(){$('.txt3').fadeIn(1000)});
